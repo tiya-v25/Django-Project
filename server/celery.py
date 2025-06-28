5af8c62c4894af563fbbaad32030574a159325de
tiya-v25 <tiyav2509@gmail.com>
2025-06-28 16:01:10 +0530

    final commit

--- server/celery.py
+++ server/celery.py
@@ -1,9 +1,16 @@
+# backbot/celery.py
 from __future__ import absolute_import, unicode_literals
 import os
 from celery import Celery
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'server.settings')
 
-app = Celery('server')
+app = Celery('backbot')
+
 app.config_from_object('django.conf:settings', namespace='CELERY')
+
 app.autodiscover_tasks()
+
+@app.task(bind=True)
+def debug_task(self):
+    print(f'Request: {self.request!r}')
--- server/settings.py
+++ server/settings.py
@@ -5,7 +5,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = config('SECRET_KEY', default='your-secret-key')
 
-DEBUG = config('DEBUG', default=True, cast=bool)
+DEBUG = config('DEBUG', default=False, cast=bool)
 
 ALLOWED_HOSTS = ['*']  
 
